8b3e27c6ee6d2e32bfbd50296214c882db7893ef
Tyler Jasper <tjasper77@icloud.com>
2016-06-14 20:10:57 -0500

    Test

--- SharkPit/AppDelegate.swift
+++ SharkPit/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        
+        print("GIT TEST")
         return true
     }
 
